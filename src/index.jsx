656769ff749a7dc10527b515a9725686ec4448f3
Matteo Pietro Dazzi <matteo.dazzi@mia-platform.eu>
2022-06-03 18:55:06 +0200

    First commit

---
+++ src/index.jsx
@@ -0,0 +1,33 @@
+/* @refresh reload */
+import { render } from 'solid-js/web';
+
+import './index.css';
+import App from './App';
+
+let appContainer
+
+function retrieveContainer (props) {
+    const {container} = props
+    return container ? container.querySelector('#root') : document.querySelector('#root')
+}
+
+function renderApp (props) {
+    appContainer = appContainer || retrieveContainer(props)
+    render(() => <App />, appContainer);
+}
+
+export async function mount (props) {
+    renderApp(props)
+}
+  
+export async function unmount (props) {
+    appContainer.textContent = ''
+}
+
+export async function bootstrap () {
+
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+    renderApp({})
+}
